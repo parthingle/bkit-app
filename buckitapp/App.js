31eec5c13e8b7727332a849d43d1eb76b58874ab
George Zhang <georgezhang@ucla.edu>
2019-04-15 21:56:08 -0700

    add rncamera

--- buckitapp/App.js
+++ buckitapp/App.js
@@ -6,62 +6,66 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import { LogingButton } from 'react-native-fbsdk'
-import { createBottomTabNavigator, createAppContainer, createSwitchNavigator } from 'react-navigation'
-
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import { LogingButton } from "react-native-fbsdk";
+import {
+  createBottomTabNavigator,
+  createAppContainer,
+  createSwitchNavigator
+} from "react-navigation";
+import { RNCamera } from "react-native-camera";
 class Test1 extends Component {
-  render(){
-    return(
-      <View style={{flex:1}}>
-          <Text>Tab 1</Text>
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text>Tab 1</Text>
       </View>
-    )
+    );
   }
 }
 
 class Test2 extends Component {
-  render(){
-    return(
-      <View style={{flex:1}}>
-          <Text>Tab 2</Text>
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text>Tab 2</Text>
       </View>
-    )
+    );
   }
 }
 const tab = createBottomTabNavigator({
   tab1: Test1,
   tab2: Test2
-})
+});
 
-const AppContainer = createAppContainer(createSwitchNavigator({
-  App:tab
-}))
+const AppContainer = createAppContainer(
+  createSwitchNavigator({
+    App: tab
+  })
+);
 type Props = {};
 export default class App extends Component<Props> {
   render() {
-    return (
-      <AppContainer/>
-    );
+    return <AppContainer />;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
