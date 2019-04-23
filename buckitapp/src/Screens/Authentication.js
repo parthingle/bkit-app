e8f4561de54aae01f8ff00db898887f4246ef2cf
Chris Lin <chrislin949@gmail.com>
2019-04-22 21:22:52 -0700

    init home page

--- buckitapp/src/Screens/Authentication.js
+++ buckitapp/src/Screens/Authentication.js
@@ -3,8 +3,11 @@ import { createStackNavigator } from "react-navigation";
 
 import MainScreen from "./Authentication/MainScreen";
 import EnterDetails from "./Authentication/EnterDetails";
+import HomeScreen from "./Home";
 
 export default createStackNavigator({
+  HomeScreen: HomeScreen,  
   MainScreen: MainScreen,
-  EnterDetails: EnterDetails
+  EnterDetails: EnterDetails,
+
 });
---
+++ buckitapp/src/Screens/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+class Home extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Home Page!</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+});
+
+export default Home;
