a5987ee22a64dd3417549b718476553d359bbe90
Chris Lin <chrislin949@gmail.com>
2019-04-29 20:44:57 -0700

    bottom drawer basic functionality

--- buckitapp/src/Screens/Home.js
+++ buckitapp/src/Screens/Home.js
@@ -1,28 +1,89 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, Image, Button } from 'react-native';
 import BottomDrawer from 'rn-bottom-drawer';
-import PullUpTab from "./List/PullUpTab";
 
 const { width, height } = Dimensions.get('window')
+const TAB_BAR_HEIGHT = 49;
+const HEADER_HEIGHT = 60;
+
 
 class Home extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Home Page!</Text>
-                <PullUpTab></PullUpTab>
-            </View>
-        );
-    }
+  renderContent = () => {
+    // let pic = {
+    //   uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    // };
+    // return(
+    //   <ScrollView horizontal={true}>
+    //     <TouchableOpacity>
+    //       <View style={styles.bottomdrawer}>
+    //         <Text>Pull Up Tab:</Text>
+    //         {/* <View>
+    //           <Image source={pic} style={{width: 110, height: 110}}/>
+    //           <Text>Impale banana on spiky tree outside bplate</Text>
+    //         </View> */}
+    //       </View>
+    //       <View style={styles.date}><Text>Today</Text></View>
+    //       <View style={styles.date}><Text>Today</Text></View>
+    //       <View style={styles.date}><Text>Today</Text></View>
+    //       <View style={styles.date}><Text>Today</Text></View>
+    //       <View style={styles.date}><Text>Today</Text></View>
+    //       <View style={styles.date}><Text>Today</Text></View>
+    //     </TouchableOpacity>
+    //   </ScrollView>
+    // )
+    return (
+      <View style={styles.contentContainer}>
+        <Text style={styles.text}>Get directions to your location</Text>
+        <View style={styles.buttonContainer}>
+          <Button title="first button" />
+          <Button title="second button" />
+        </View>
+      </View>
+    )
+  }
+  render() {
+    return (
+      <View>
+        <Text>Home Page!</Text>
+        <BottomDrawer
+          containerHeight={height}
+          downDisplay={height/2}
+          offset={TAB_BAR_HEIGHT + HEADER_HEIGHT}
+          onExpanded = {() => {console.log('expanded')}}
+          onCollapsed = {() => {console.log('collapsed')}}
+          startUp={false}
+        >
+          {this.renderContent()}
+        </BottomDrawer>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  bottomdrawer: {
+    backgroundColor: '#F5FCAA'
+  },
+  date: {
+    backgroundColor: '#F5FDBA'
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+  text: {
+    paddingHorizontal: 5
+  }
 });
 
 export default Home;
--- buckitapp/src/Screens/List/PullUpTab.js
+++ buckitapp/src/Screens/List/PullUpTab.js
@@ -1,31 +1,20 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
-import bd from 'rn-bottom-drawer'
+// import React, { Component } from 'react';
+// import { View, Text, StyleSheet, Image } from 'react-native';
+// import BottomDrawer from 'rn-bottom-drawer';
 
-class PullUpTab extends Component {
-    render() {
-        let pic = {
-            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-        };
-        return (
-            <View style={styles.container}>
-                <Text>Pull Up Tab:</Text>
-                <View>
-                    <Image source={pic} style={{width: 110, height: 110}}/>
-                    <Text>Impale banana on spiky tree outside bplate</Text>
-                </View>
-            </View>
-        );
-    }
-}
+// const TAB_BAR_HEIGHT = 49;
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#96ffcf',
-    },
-});
+// class PullUpTab extends Component {
 
-export default PullUpTab;
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         backgroundColor: '#96ffcf',
+//     },
+// });
+
+// export default PullUpTab;
