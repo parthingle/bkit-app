8f0201e9cd61c20370c68a3159f1f1d23123dcf6
Parth Ingle <parthingle@g.ucla.edu>
2019-06-03 20:18:16 -0700

    login flow fix

--- buckitapp/src/Screens/Onboarding/FacebookLogin.js
+++ buckitapp/src/Screens/Onboarding/FacebookLogin.js
@@ -7,90 +7,56 @@ import * as axios from "axios";
 import AsyncStorage from "@react-native-community/async-storage";
 
 // import FBLoginButton from '../../Components/FBLoginButton'
-import { LoginButton } from "react-native-fbsdk";
-
-const { AccessToken } = FBSDK;
+import { LoginButton, LoginManager, AccessToken } from "react-native-fbsdk";
+import { __asyncDelegator } from "tslib";
 
 export default class FacebookLogin extends Component {
-  constructor(props) {
-    super(props);
-    this.onLoginFinished = this.onLoginFinished.bind(this);
-  }
-
-  async authFacebook() {
-    const data = await AccessToken.getCurrentAccessToken();
-    console.log("authFacebook");
-    if (!data) {
-      // No access token, wait for Facebook login
-      console.log("no access token, waiting for Facebook login");
-      return;
+    constructor(props) {
+        super(props);
     }
-    const access_token = data.accessToken;
-
-    // TODO: globalize the instance
-    const instance = axios.create({
-      baseURL: "http://localhost:8080"
-    });
-    const params = { access_token };
-
-    try {
-      console.log("GET /auth/facebook");
-      console.log(params);
-      const res = await instance.get("/auth/facebook", { params });
-      if (res.status === 202) {
-        // User does not exist: sending to sign up page
-        this.props.navigation.navigate("SignUp");
-        console.log("nav to signup");
-      } else if (res.status === 201) {
-        // User exists: sending to Main App
-        // TODO: load user data
-        await AsyncStorage.setItem("@jwtoken", res.jwtoken);
-        this.props.navigation.navigate("Main");
-        console.log("nav to main");
-      } else {
-        console.log("no nav");
-      }
-    } catch (err) {
-      console.log("/auth/facebook: error");
+    authFacebook = async () => {
+        LoginManager.logInWithReadPermissions(["public_profile"]).then(res => {
+            if (res.isCancelled) {
+                return;
+            }
+            AccessToken.getCurrentAccessToken().then(data => {
+                axios
+                    .post("http://localhost:8080/auth/facebook", {
+                        access_token: data.accessToken
+                    })
+                    .then(res => {
+                        if (res.status === 200) {
+                            this.props.navigation.navigate("Home");
+                        } else {
+                            alert(res.status);
+                        }
+                    })
+                    .catch(error => alert(error));
+            });
+        });
+    };
+    componentDidMount() {}
+    render() {
+        return (
+            <View style={{ flex: 1, backgroundColor: "#FEFDF4" }}>
+                <View style={{ paddingTop: "30%" }} />
+                <Logo style={{ top: 30 }} />
+                <View style={styles.buttonBox}>
+                    {/* <LoginButton onLoginFinished={this.onLoginFinished} /> */}
+                </View>
+                <Bars onAuth={this.authFacebook} />
+            </View>
+        );
     }
-  }
-
-  componentDidMount() {
-    //this.authFacebook();
-    console.log("FacebookLogin");
-    this.authFacebook();
-  }
-
-  onLoginFinished(error, result) {
-    if (error) {
-      alert("Login failed with error: " + error.message);
-    } else if (result.isCancelled) {
-      alert("Login was cancelled");
-    } else {
-      this.authFacebook();
-    }
-  }
-
-  render() {
-    return (
-      <View style={{ flex: 1, backgroundColor: "#FEFDF4" }}>
-        <Logo style={{ top: 30 }} />
-        <View style={styles.buttonBox}>
-          <LoginButton onLoginFinished={this.onLoginFinished} />
-        </View>
-        <Bars />
-      </View>
-    );
-  }
 }
 
 var styles = StyleSheet.create({
-  buttonBox: {
-    top: 300,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  centerBox: {
-    position: "relative"
-  }
+    buttonBox: {
+        top: 300,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    centerBox: {
+        position: "relative"
+    }
 });
