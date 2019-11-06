2a04b9e86585b0cf9c661d988c2c0f233100fe2f
Christopher Aziz <caziz@ucla.edu>
2019-11-06 10:50:32 -0800

    Replace settings page with alert message (#15)

--- buckitapp/src/MainNavigator.js
+++ buckitapp/src/MainNavigator.js
@@ -1,7 +1,6 @@
 import Home from "./Screens/Home/HomeScreen";
 import ItemScreen from "./Screens/ItemScreen";
 import { createStackNavigator } from "react-navigation";
-import Settings from "./Screens/Settings";
 
 const AppTab = createStackNavigator(
   {
@@ -10,12 +9,6 @@ const AppTab = createStackNavigator(
     },
     ItemScreen: {
       screen: ItemScreen
-    },
-    Settings: {
-      screen: Settings,
-      navigationOptions: {
-        title: "float"
-      }
     }
   },
   {
